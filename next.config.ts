6b6d6e858ec388770dc39d73f0807d9940d24f30
Петър Стоев <petarstoev0505@gmail.com>
2025-01-18 17:04:55 +0200

    feat: build config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: { unoptimized: true },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
