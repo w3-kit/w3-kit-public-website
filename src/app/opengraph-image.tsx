5307cef51849357568d12f436dd20625b1c03278
Petar Stoev <petarstoev0505@gmail.com>
2026-03-20 16:36:11 +0200

    feat(seo): add dynamic OG images for root and all component pages

---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,8 @@
+import { generateRootOgImage, OG_SIZE, OG_CONTENT_TYPE } from "@/lib/og-image";
+
+export const size = OG_SIZE;
+export const contentType = OG_CONTENT_TYPE;
+
+export default function Image() {
+  return generateRootOgImage();
+}
